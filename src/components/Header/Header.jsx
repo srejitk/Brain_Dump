b5f3bc1e8923f0299ae2818651f35ad5f3f4432e
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-05 23:44:20 +0530

    Feat : Added Header Component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { useState } from "react/cjs/react.development";
+
+export default function Header() {
+  const [showOption, setShowOption] = useState(false);
+
+  const optionHandler = () => {
+    setShowOption(!showOption);
+  };
+
+  console.log(showOption);
+  return (
+    <div
+      className={`flex-row-wrap position-relative box-shadow ${styles.Navbar}`}
+    >
+      <div className={`flex-row-wrap gap20 ${styles.Navbar_brand}`}>
+        <span className="material-icons">menu</span>
+
+        <Link to="/" className={`flex-row-wrap gap20 ${styles.Navbar_brand}`}>
+          <div className={`flex-mid-center ${styles.logo_container}`}>
+            <img
+              className={styles.logo}
+              src="https://res.cloudinary.com/dkqrmlxlg/image/upload/v1649100763/Brain%20Dump/Brain_dlnboa.svg"
+              alt="BrainDump Logo"
+            />
+          </div>
+          <h5 className="header-5">BrainDump</h5>
+        </Link>
+      </div>
+      <div className={`flex-row-wrap flex-mid-center gap20`}>
+        <Link to="/login" className="links subtitle-1">
+          Sign In
+        </Link>
+        <Link to="sign-up" className="links subtitle-1">
+          Sign Up
+        </Link>
+        <Link to="sign-up" className="links subtitle-1">
+          Logout
+        </Link>
+        <Link
+          to="/"
+          className={`btn_action btn btn--small`}
+          onClick={optionHandler}
+        >
+          <span className="material-icons">face</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
