e4a72c3c331268ea8dc55261474987bfb03be360
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-07 21:08:55 +0530

    Feat : Added Trash Page

---
+++ src/components/Trash/Trash.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useNote } from "../../contexts/Note/NoteContext";
+import EmptyState from "../EmptyState/EmptyState";
+import NoteCard from "../Note/Note";
+import styles from "./Trash.module.css";
+
+export default function Trash() {
+  const { noteState } = useNote();
+  const { trashedNotes } = noteState;
+
+  return (
+    <div
+      className={`${styles.noteslist} grid position-relative ${
+        trashedNotes?.length === 0 ? styles.single_grid : ""
+      }`}
+    >
+      {trashedNotes?.length === 0 ? (
+        <div className="flex-mid-center">
+          <EmptyState
+            stateTitle={"No trash here."}
+            stateDesc="You have nothing to hide huh"
+            btnText="Go Home"
+            icon={"delete"}
+            endpoint="/"
+            color={"all-yellow"}
+          />
+        </div>
+      ) : (
+        <h3 className={`${styles.pageTitle} header-4 position-absolute`}>
+          {`Trashed Notes`}
+        </h3>
+      )}
+      {trashedNotes?.map((note) => (
+        <NoteCard key={note._id} note={note} />
+      ))}
+    </div>
+  );
+}
