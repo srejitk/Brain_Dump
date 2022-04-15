c101e7f8209ca7218e380c00bec4f1f2b5c8fee1
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-16 00:29:46 +0530

    Refactor : Label Selector Component

--- src/components/LabelSelector/LabelSelector.jsx
+++ src/components/LabelSelector/LabelSelector.jsx
@@ -5,25 +5,48 @@ import { useNote } from "../../contexts/Note/NoteContext";
 
 export default function LabelSelector() {
   const [label, setlabel] = useState("");
+  const [showForm, setShowForm] = useState(true);
   const { note, setNote } = useNote();
+  const clickHandler = () => {
+    setShowForm((prev) => !prev);
+  };
 
   return (
-    <div className={`$flex-row-wrap  box-shadow ${styles.labelSelector}`}>
+    <div
+      className={`flex-row-wrap  ${
+        showForm ? styles.showSelector : styles.hideSelector
+      }  box-shadow ${styles.labelSelector}`}
+    >
+      <div
+        className="btn_action btn btn--small"
+        style={
+          !showForm
+            ? { backgroundColor: `var(--component-red-02)` }
+            : { backgroundColor: `var(--CARD_BG)` }
+        }
+        onClick={clickHandler}
+      >
+        <span className="material-icons">label</span>
+      </div>
       <input
         type="text"
-        className={styles.labelInput}
+        className={`${styles.labelInput} ${
+          !showForm ? styles.showInput : styles.hideInput
+        }`}
         onChange={(e) => setlabel(e.target.value)}
         value={label}
         placeholder="New Label"
       />
       <button
         onClick={() => {
-          setNote({ ...note, label: [label] });
+          setNote({ ...note, label: [...note.label, label] });
           setlabel("");
         }}
-        className={`${styles.add_label} flex-mid-center`}
+        className={`${styles.add_label} ${
+          !showForm ? styles.showInput : styles.hideInput
+        } btn btn_action btn--small flex-mid-center`}
       >
-        +
+        <span className="flex-mid-center material-icons">add</span>
       </button>
     </div>
   );
