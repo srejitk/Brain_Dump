e8deb45e123b22937374bf03e62f0e38e4e8835d
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-16 00:29:22 +0530

    Refactor : ColorPallete Component

--- src/components/ColorPallete/ColorPallete.jsx
+++ src/components/ColorPallete/ColorPallete.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNote } from "../../contexts/Note/NoteContext";
-import { noteReducer } from "../../contexts/Note/NoteReducer";
 import styles from "./ColorPallete.module.css";
 
 export default function ColorPallete() {
   const { note, setNote } = useNote();
+  const [showForm, setShowForm] = useState(false);
+
   const pallete = [
     `var(--white)`,
     `var(--component-blue-02)`,
@@ -12,14 +13,35 @@ export default function ColorPallete() {
     `var(--component-green-02)`,
     `var(--component-yellow-04)`,
   ];
+
+  const clickHandler = () => {
+    setShowForm((prev) => !prev);
+  };
   return (
     <div className={`${styles.ColorPallete} flex-row-wrap box-shadow`}>
-      <div className={`${styles.pallete_selector} flex-row-wrap `}>
+      <div
+        className={`${styles.pallete_selector} ${
+          !showForm ? styles.showSelector : styles.hideSelector
+        } flex-row-wrap `}
+      >
+        <div
+          className="btn_action btn btn--small"
+          style={
+            showForm
+              ? { backgroundColor: `var(--component-orange-02)` }
+              : { backgroundColor: `var(--CARD_BG)` }
+          }
+          onClick={clickHandler}
+        >
+          <span className="material-icons">color_lens</span>
+        </div>
         {pallete.map((color) => {
           return (
             <span
               key={color}
-              className={styles.color}
+              className={`${styles.color}  flex-mid-center ${
+                showForm ? styles.showPallete : styles.hidePallete
+              } material-icons`}
               style={{
                 backgroundColor: color,
                 border:
@@ -29,7 +51,9 @@ export default function ColorPallete() {
               onClick={() => {
                 setNote({ ...note, color: color });
               }}
-            ></span>
+            >
+              {note.color === color ? `done` : ""}
+            </span>
           );
         })}
       </div>
