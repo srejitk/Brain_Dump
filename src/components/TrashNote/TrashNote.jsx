17100c561575294ad417a29e4c34fff1f12597f5
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-19 03:36:04 +0530

    Refactor : Updated Trash NoteCard

--- src/components/TrashNote/TrashNote.jsx
+++ src/components/TrashNote/TrashNote.jsx
@@ -2,75 +2,98 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNote } from "../../contexts/Note/NoteContext";
 import styles from "./TrashNote.module.css";
-
+import DOMPurify from "dompurify";
 export default function TrashNote({ note }) {
-  const { title, notes } = note;
+  const { title, notes, timestamp, color, label } = note;
   const [options, setOptions] = useState(false);
-  const { noteState, setNote, noteDispatch, archiveNote, RestoreArchivedNote } =
-    useNote();
+  const {
+    noteState,
+    noteDispatch,
+    deleteNote,
+    restoreArchivedNote,
+    deleteArchivedNote,
+  } = useNote();
 
-  const { color, label, day, timestamp } = note;
-
-  const deleteFromTrash = (note) => {
-    noteDispatch({ type: "DELETE_FROM_TRASH", payload: note });
-  };
   const optionsHandler = () => {
     setOptions((prev) => !prev);
   };
 
+  const deleteHandler = (note) => {
+    deleteNote(note, noteDispatch);
+    noteState.archivedNotes.filter((item) => item._id !== note._id);
+  };
+
   return (
     <div
-      className={`${styles.note} border-radius box-shadow position-relative`}
+      className={`${styles.note} ${
+        options ? styles.enlargeNote : styles.defaultNote
+      } border-radius box-shadow position-relative`}
       style={{ backgroundColor: `${color}` }}
     >
-      <div onClick={optionsHandler}>
-        <span className={`position-absolute  ${styles.options} material-icons`}>
-          more_horiz
-        </span>
-      </div>
       <div className={`${styles.note_content} flex-column-wrap flex-top-left`}>
         <h3 className={styles.note_title}>{title}</h3>
         <p
           className={styles.note_body}
           dangerouslySetInnerHTML={{
-            __html: notes,
+            __html: DOMPurify.sanitize(notes),
           }}
         ></p>
       </div>
-
-      {options && (
-        <div
-          className={`${styles.note_footer} flex-row-wrap position-absolute box-shadow`}
-        >
-          <span
-            className={`position-absolute ${styles.appliedLabel}`}
-            style={{ backgroundColor: color }}
-          >
-            {note.label ? "Work" : label}
-          </span>
-          <Link
-            to="/"
-            className={`btn_action btn btn--small`}
-            onClick={() => archiveNote(note, noteDispatch)}
-          >
-            <span className={`material-icons`}>restore</span>
-          </Link>
-          <Link
-            to="/"
-            className={`btn_action btn btn--small`}
-            onClick={() => deleteFromTrash(note)}
-          >
-            <span className={`material-icons`}>delete</span>
-          </Link>
-        </div>
-      )}
-      <div className="flex-row-wrap flex-left-center">
+      <div onClick={optionsHandler}>
         <span
-          className={`${styles.createdDate}`}
+          className={`position-absolute  ${styles.options} material-icons`}
           style={{ backgroundColor: color }}
         >
-          {timestamp.slice(4, 10)}
+          more_horiz
         </span>
+      </div>
+
+      {
+        <div
+          className={`${styles.note_footer} ${
+            options ? styles.enlargeOption : styles.hideOption
+          } flex-column-wrap position-absolute box-shadow`}
+        >
+          <Link
+            to="/"
+            className={`btn_action btn btn--small`}
+            onClick={() =>
+              noteDispatch({ type: "RESTORE_FROM_TRASH", payload: note })
+            }
+          >
+            {" "}
+            <span className={`material-icons`}>restore_from_trash</span>
+          </Link>
+          <Link
+            to="/"
+            className={`btn_action btn btn--small`}
+            onClick={() =>
+              noteDispatch({ type: "DELETE_FROM_TRASH", payload: note })
+            }
+          >
+            <span className={`material-icons`}>delete_forever</span>
+          </Link>
+        </div>
+      }
+      {label?.map((item) => {
+        return (
+          <span
+            className={`flex-row-wrap ${styles.appliedLabel}`}
+            style={{ backgroundColor: color }}
+          >
+            {item}
+          </span>
+        );
+      })}
+      <div className={`flex-row-wrap  ${styles.info_container}`}>
+        <div className="flex-row-wrap flex-left-center">
+          <span
+            className={`${styles.createdDate}`}
+            style={{ backgroundColor: color }}
+          >
+            {timestamp?.slice(0)}
+          </span>
+        </div>
       </div>
     </div>
   );
